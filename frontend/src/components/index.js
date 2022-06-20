bfcfb6007df5f699998e4ead3e0d18c7678a7c10
bhavya1022 <shethb2210@gmail.com>
2022-06-20 16:58:04 +0530

    'RawData'

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,6 @@ import Login from "./Login/LoginForm";
 import ForgotPassword from "./ForgotPassword/ForgotPassword";
 import Navbar from "./Navbar/Navbar";
 import Leads from "./Navbar/Leads";
+import RawData from "./RawData/RawData";
 
-export { Register, Login, ForgotPassword ,Navbar,Leads};
+export { Register, Login, ForgotPassword ,Navbar,Leads, RawData};
