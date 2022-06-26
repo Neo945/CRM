cd2e3993343ae996698b5bff1e8831930af1a695
Chintan <chintanshah2587@gmail.com>
2022-06-27 00:08:53 +0530

    Pre sales integration

--- frontend/src/components/SalesPage/PreSales.js
+++ frontend/src/components/SalesPage/PreSales.js
@@ -1,88 +1,100 @@
+import React, { Component, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { lookup } from "../../utils";
+
 import React, { Component } from "react";
 import ReactTable from "react-table-6";
 import "react-table-6/react-table.css";
 import CopyNav from '../CopyNav/CopyNav';
  import Footer from "../Footer/Footer";
-class PreSales extends Component {
-
-  render() {
-    const data = [
-        {
-            Salesid:"1001",
-            PreSalesid:"1111",
-            Name: "Ayaan",
-            Email:"abc@gmail.com",
-            Phone: 7419638521,
-            ProposalDetails: "xyz",
-            ProposalDates: "xyz",
-            Create:"CRM",
-            Createdby:"any"
-          },
-    ];
-    const columns = [
-        {
-            Header: "Sales id",
-            accessor: "Salesid",
-          },
-          {
-            Header: "PreSales id",
-            accessor: "PreSalesid",
-          },
-          {
-            Header: "Name",
-            accessor: "Name",
-          },
-          {
-            Header: "Email",
-            accessor: "Email",
-          },
-          {
-            Header: "Phone",
-            accessor: "Phone",
-          },
-          {
-            Header: "ProposalDetails",
-            accessor: "ProposalDetails",
-          },
-          {
-             Header: "Proposal Dates",
-            accessor: "ProposalDates",
-          },
-          {
-            Header: "Created On",
-            accessor: "Create",
-          },
-          {
-            Header: "Created by",
-            accessor: "Createdby",
-          },
-    ];
-    
-    return (
-     
-      <div>
-        <CopyNav/>
-        <ReactTable
-          data={data}
-          columns={columns}
-          defaultPageSize={10}
-          pageSizeOptions={[2, 4, 6]}
-      />
-<br></br>
-
-
-        <select style={{ display: "block" }}>
-          <option value="All">All</option>
-          <option value="Checked">Checked</option>
-          <option value="Unhecked">Unhecked</option>
-        </select>
-
-        <br></br>
-
-<Footer></Footer>
-      </div>
-
+ function PreSales(props) {
+  let { jobid } = useParams();
+  const [data, setData] = React.useState([]);
+  const [checked, setChecked] = React.useState("All");
+  useEffect(() => {
+    lookup("GET", `/leads/presales/job/${jobid}`, "", null).then(
+      ({ data, status }) => {
+        if (status === 200) {
+          console.log(data);
+          setData(data.data);
+        }
+      }
     );
-  }
+  }, []);
+  const columns = [
+    {
+      Header: "ID",
+      accessor: "salesleads.marketinglead.leads.customer.id",
+    },
+    {
+      Header: "Name",
+      accessor: "salesleads.marketinglead.leads.customer.name",
+    },
+    {
+
+      Header: "IS Done",
+      accessor: "salesleads.marketinglead.leads.customer.is_done",
+    },
+    {
+      Header: "Approved by",
+      accessor: "approved_by.first_name",
+    },
+    {
+      Header: " Sales Leads ",
+      accessor: "salesleads.marketinglead.leads.customer.saleslead",
+    },
+    {
+      Header: "Date Created",
+      accessor: "date_created",
+    },
+    {
+      Header: "Updated date",
+      accessor: "salesleads.marketinglead.leads.customer.date_updated",
+    },
+    {
+      Header: "Proposal details",
+      accessor: "salesleads.marketinglead.leads.customer.proposal_details",
+    },
+    {
+      Header: "Proposal Dates",
+      accessor: "salesleads.marketinglead.leads.customer.proposal_date",
+    },
+ 
+    // {
+    //   Header: "FeedBack",
+    //   accessor: "cmrcss",
+    // },
+  ];
+
+  return (
+    <div className="table">
+      <CopyNav />
+      <ReactTable
+        data={
+          checked === "All"
+            ? data
+            : checked === "Checked"
+            ? data.filter((x) => x.is_done)
+            : data.filter((x) => !x.is_done)
+        }
+        columns={columns}
+        defaultPageSize={10}
+        pageSizeOptions={[2, 4, 6]}
+      />
+      <br></br>
+      <select
+        style={{ display: "block" }}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setChecked(e.target.value);
+        }}
+      >
+        <option value="All">All</option>
+        <option value="Checked">Checked</option>
+        <option value="Unhecked">Unhecked</option>
+      </select>
+      <Footer></Footer>
+    </div>
+  );
 }
 export default PreSales;
